014906b1ed24c331e8ab43ce6b46f473b638744c
unknown <20193tn108@utez.edu.mx>
2022-05-18 17:50:22 -0500

    Correccion de errores

--- UnidadI-AguasJackeline/src/main/kotlin/ProgramingCourse.kt
+++ UnidadI-AguasJackeline/src/main/kotlin/ProgramingCourse.kt
@@ -1,22 +1,22 @@
 import java.util.*
 class ProgramingCourse (id : Int, name : String, description : String) : Course (id, name, description) {
 
-    fun addProgramingCourse(programingCourse: ProgramingCourse, listProgramingCourse: MutableList<ProgramingCourse>) : MutableList<ProgramingCourse>{
+    open fun addProgramingCourse(programingCourse: ProgramingCourse, listProgramingCourse: MutableList<ProgramingCourse>) : MutableList<ProgramingCourse>{
         listProgramingCourse.add(programingCourse)
         return listProgramingCourse;
     }
 
-    fun deleteProgramingCourse(programingCourse: ProgramingCourse, listProgramingCourse: MutableList<ProgramingCourse>) : MutableList<ProgramingCourse>{
+    open fun deleteProgramingCourse(programingCourse: ProgramingCourse, listProgramingCourse: MutableList<ProgramingCourse>) : MutableList<ProgramingCourse>{
         listProgramingCourse.remove(programingCourse)
         return listProgramingCourse
     }
 
-    fun updateProgramingCourse(programingCourse: ProgramingCourse, listProgramingCourse: MutableList<ProgramingCourse>, indice : Int) : MutableList<ProgramingCourse>{
+    open fun updateProgramingCourse(programingCourse: ProgramingCourse, listProgramingCourse: MutableList<ProgramingCourse>, indice : Int) : MutableList<ProgramingCourse>{
         listProgramingCourse[indice] = programingCourse
         return listProgramingCourse;
     }
 
-    fun getProgramingCourse(listProgramingCourse: MutableList<ProgramingCourse>, indice : Int) : ProgramingCourse{
+    open fun getProgramingCourse(listProgramingCourse: MutableList<ProgramingCourse>, indice : Int) : ProgramingCourse{
         return listProgramingCourse[indice];
     }
 
