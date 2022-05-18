2a1b2931cf1a8963a37edac669ab176ff3d261bc
unknown <20193tn108@utez.edu.mx>
2022-05-18 17:46:09 -0500

    ProgramingCourse

--- UnidadI-AguasJackeline/src/main/kotlin/ProgramingCourse.kt
+++ UnidadI-AguasJackeline/src/main/kotlin/ProgramingCourse.kt
@@ -1,5 +1,23 @@
+import java.util.*
 class ProgramingCourse (id : Int, name : String, description : String) : Course (id, name, description) {
 
+    fun addProgramingCourse(programingCourse: ProgramingCourse, listProgramingCourse: MutableList<ProgramingCourse>) : MutableList<ProgramingCourse>{
+        listProgramingCourse.add(programingCourse)
+        return listProgramingCourse;
+    }
 
+    fun deleteProgramingCourse(programingCourse: ProgramingCourse, listProgramingCourse: MutableList<ProgramingCourse>) : MutableList<ProgramingCourse>{
+        listProgramingCourse.remove(programingCourse)
+        return listProgramingCourse
+    }
+
+    fun updateProgramingCourse(programingCourse: ProgramingCourse, listProgramingCourse: MutableList<ProgramingCourse>, indice : Int) : MutableList<ProgramingCourse>{
+        listProgramingCourse[indice] = programingCourse
+        return listProgramingCourse;
+    }
+
+    fun getProgramingCourse(listProgramingCourse: MutableList<ProgramingCourse>, indice : Int) : ProgramingCourse{
+        return listProgramingCourse[indice];
+    }
 
 }
